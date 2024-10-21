608b624b6c0ea5e1bf95c66f6fca9cc4543a6439
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-21 15:43:19 +0300

    Task 3

---
+++ Tyuiu.StoyakinNM.Sprint3.Task3.V26/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.StoyakinNM.Sprint3.Task3.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach подсчитать количество букв “e” в строке: have    *");
+        Console.WriteLine("* a nice time.                                                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string value = "have as nice time";
+        char chr = 'e';
+
+        Console.WriteLine("Исходная строка = " + value);
+        Console.WriteLine("Исходный символ = " + chr);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Количество символов = " + ds.GetCharCount(value, chr));
+        Console.ReadKey();
+    }
+}
