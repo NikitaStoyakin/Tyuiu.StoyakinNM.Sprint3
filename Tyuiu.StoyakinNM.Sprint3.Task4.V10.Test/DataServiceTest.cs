c361d282970efea88b37f16a4242be308be52606
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-22 11:49:23 +0300

    Task 4

---
+++ Tyuiu.StoyakinNM.Sprint3.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+using Tyuiu.StoyakinNM.Sprint3.Task4.V10.Lib;
+namespace Tyuiu.StoyakinNM.Sprint3.Task4.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int StartValue = -5;
+            int StopValue = 5;
+
+            double res = ds.Calculate(StartValue, StopValue);
+
+            double wait = -0.117;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.StoyakinNM.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.StoyakinNM.Sprint3.Task4.V10.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+        Console.WriteLine("* функции y=(sin(x)-x)/x.  При х = 0 пропустить значение Полученные       *");
+        Console.WriteLine("* значения перемножать.                                                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int StartValue = -5;
+        int StopValue = 5;
+
+        Console.WriteLine("Старт шага = " + StartValue);
+        Console.WriteLine("Конец шага = " + StopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение ряда = " + ds.Calculate(StartValue, StopValue));
+        Console.ReadKey();
+    }
+}
