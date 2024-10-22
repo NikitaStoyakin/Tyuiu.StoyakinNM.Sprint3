90326e226dae3fe4ef2ef0321d4b672d4dc1630a
Neority <Tyuiu.StoyakinNM@outlook.com>
2024-10-22 12:35:08 +0300

    Task 5

---
+++ Tyuiu.StoyakinNM.Sprint3.Task5.V21.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.StoyakinNM.Sprint3.Task5.V21.Lib
+{
+    public class DataService : ISprint3Task5V10
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeries = 0;
+            int i, k;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (k = startValue2; k <= stopValue2; k++)
+                {
+                    sumSeries = sumSeries + ((Math.Pow(x, 3) * Math.Cos(startValue2)) + 2);
+                }
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.StoyakinNM.Sprint3.Task5.V21/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.StoyakinNM.Sprint3.Task5.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Стоякин Н.М. | ПКТб-24-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Стоякин Никита Михайлович | ПКТб 24-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*      3   10    3                                                        *");
+        Console.WriteLine("* Y = Sum  Sum (X  * Cos(k)) + 2 при X = 2                                *");
+        Console.WriteLine("*     i=1  k=1                                                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int x = 2;
+        int startValue1 = 1;
+        int startValue2 = 1;
+        int stopValue1 = 3;
+        int stopValue2 = 10;
+
+        Console.WriteLine("Переменная X = " + x);
+        Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+        Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+        Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+        Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+        Console.ReadKey();
+    }
+}
